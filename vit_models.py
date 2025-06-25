22c7b64881b74292c2516eddc86a7e9d10cfaccf
Capsaicinno <isshuf@gmail.com>
2025-06-25 18:43:05 +0900

    made jupyternotebook

--- vit_models.py
+++ vit_models.py
@@ -1,1 +1,12 @@
 #まとめ用
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from torch.nn import CrossEntropyLoss
+from torch.utils.data import DataLoader
+
+from torchvision.transforms import ToTensor
+from torchvision.datasets.mnist import MNIST
+
+np.random.seed(0)
+torch.manual_seed(0)
